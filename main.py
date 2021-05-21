d846b940c7619549b53349770da4305951539f26
Michele Pagano <75363823+AlmondFlame1287@users.noreply.github.com>
2021-05-21 11:23:24 +0200

    Added commands

--- main.py
+++ main.py
@@ -1,41 +1,78 @@
-import discord
 from discord.ext.commands import Bot
-import time as t
-client = discord.Client()
+import os
 
-bot = Bot(command_prefix="!")
-
-@client.event
-async def on_message(message):
-
-    if message.author == client.user:
-        return
-
-    if message.content.startswith("Dib"):
-        await message.channel.send("finocchio")
-
-    if message.content.startswith("Python"):
-        await message.channel.send("Minchia cazz mi chiami a fare dio can")
-
-    if message.content.startswith("Minecraftata?"):
-        await message.channel.send("Ehhhh bohhhhh")
-
-    if message.content.startswith("We fratmo come stai?"):
-        await message.channel.send("T'appost, a te?")
-
-    if message.content.startswith("Ehy tu, vuoi fare una call"):
-        t.sleep(0.4)
-        await message.channel.send("No fra guarda voglio segarmi")
-
-    if message.content.startswith("Non dire ste cose"):
-        t.sleep(0.4)
-        await message.channel.send("Ma come? Non le dici pure tu?")
-
-    if message.content.startswith("no") or message.content.startswith("No"):
-        await message.channel.send("Eccolo quel mongoloide di merda che non vuole"
-                                   "mai fare un cazzo")
+comandi = ['aiuto', 'bw', 'minecraft', 'dib', 'pag', 'gruz', 'server']
+bot = Bot(command_prefix="!", case_insensitive=True)
 
 
-@bot.command(name = "dio")
-async def send_message(message):
-    await message.channel.send("Borco Bio")
+@bot.event
+async def on_ready():
+    print("so lillo")
+
+# Help
+
+
+@bot.command(name = comandi[0])
+async def aiu(ctx):
+    await ctx.send(comandi)
+
+# Bedwars
+
+
+@bot.command(name = comandi[1])
+async def bw(ctx):
+    await ctx.send('@everyone Bedwarsina?')
+
+# Minecraft
+
+
+@bot.command(name = comandi[2])
+async def mc(ctx):
+    await ctx.send('@everyone Vuoi giocare a minecraft?')
+
+# Dib
+
+
+@bot.command(name = comandi[3])
+async def db(ctx):
+    await ctx.send('Gruz sei proprio inaffidabile')
+
+# Pagano
+
+
+@bot.command(name = comandi[4])
+async def pg(ctx):
+    await ctx.send('Nu pazzariell allor ja')
+
+# Grusio
+
+
+@bot.command(name = comandi[5])
+async def gruz(ctx):
+    await ctx.send('Che finocchio!!')
+
+# Server
+
+
+@bot.command(name = comandi[6])
+async def serv(ctx):
+    await ctx.send('Grusio, apri sto cazz di server')
+
+# Posaman
+
+
+@bot.command(name = 'lillo')
+async def lillo(ctx):
+    await ctx.send('https://www.youtube.com/watch?v=jZbUnwjEcz0&ab_channel=AtleticoVescovioAtleticoVescovio')
+    await ctx.send('so lillo')
+
+# Arrotino
+
+
+@bot.command(name = 'arrotino')
+async def arro(ctx):
+    await ctx.send('https://www.youtube.com/watch?v=MKsxkO9qn_Q&ab_channel=SocialStories')
+    await ctx.send("Sveglia donne, è arrivato l'arrotino")
+
+
+bot.run(os.getenv('TOKEN'))
