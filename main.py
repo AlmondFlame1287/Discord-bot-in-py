d036af68351aefa0354adc179491c5fa71fd0293
Michele Pagano <>
2021-05-22 12:54:07 +0000

    Added significative comments and Test Zone

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import discord
 from discord.ext.commands import Bot
 import os
 
@@ -5,64 +6,68 @@ import os
 token = os.environ['TOKEN']
 
 
-comandi = ['aiuto', 'bw', 'minecraft', 'dib', 'pag', 'gruz', 'server', 'lillo', 'arrotino', 'ale', 'skammettino', 'tommasino']
+comandi = ['aiuto', 'bw', 'minecraft', 'inaffidabile', 'pazzo', 'fin', 'server', 'lillo', 'arrotino', 'amo', 'fratm', 'schiavo']
+
+
+test = ['gioco1']
+
+
 bot = Bot(command_prefix="!", case_insensitive=True)
-
-
 @bot.event
 async def on_ready():
     print("so lillo")
 
-# Help
+# Aiuto generale
 
 
 @bot.command(name = comandi[0])
 async def aiu(ctx):
     await ctx.send(comandi)
 
-# Bedwars
+
+# Chiedi per una bedwars
 
 
 @bot.command(name = comandi[1])
 async def bw(ctx):
     await ctx.send('@everyone Bedwarsina?')
 
-# Minecraft
+# Chiedi per giocare a minecraft
 
 
 @bot.command(name = comandi[2])
 async def mc(ctx):
     await ctx.send('@everyone Vuoi giocare a minecraft?')
 
-# Dib
+# {member.mention} è inaffidabile
 
 
 @bot.command(name = comandi[3])
-async def db(ctx):
-    await ctx.send('Gruz sei proprio inaffidabile')
+async def inaff(ctx, member : discord.Member):
+    await ctx.send(f'{member.mention} sei proprio inaffidabile')
 
-# Pagano
+# Frase tipica
 
 
 @bot.command(name = comandi[4])
-async def pg(ctx):
+async def pazzo(ctx):
     await ctx.send('Nu pazzariell allor ja')
 
-# Grusio
+# Frase tipica di G.
 
 
 @bot.command(name = comandi[5])
-async def gruz(ctx):
+async def fino(ctx):
     await ctx.send('Che finocchio!!')
 
-# Server
+# Chiedi di aprire un server di minecraft
 
 
 @bot.command(name = comandi[6])
-async def serv(ctx):
-    await ctx.send('Grusio, apri sto cazz di server')
+async def serv(ctx, member : discord.Member):
+    await ctx.send(f'{member.mention} apri sto cazz di server')
 
-# Posaman
+# Chiama lillo
 
 
 @bot.command(name = comandi[7])
@@ -70,7 +75,7 @@ async def lillo(ctx):
     await ctx.send('https://www.youtube.com/watch?v=jZbUnwjEcz0&ab_channel=AtleticoVescovioAtleticoVescovio')
     await ctx.send('so lillo')
 
-# Arrotino
+# Chiama l'arrotino
 
 
 @bot.command(name = comandi[8])
@@ -78,25 +83,39 @@ async def arro(ctx):
     await ctx.send('https://www.youtube.com/watch?v=MKsxkO9qn_Q&ab_channel=SocialStories')
     await ctx.send("Sveglia donne, è arrivato l'arrotino")
 
-# Zi
+# {member.mention} ti si vuole bene
 
 
 @bot.command(name = comandi[9])
-async def zi(ctx):
-    await ctx.send('Bella zi, te se ama')
+async def te_se_ama(ctx, member : discord.Member):
+    await ctx.send(f'Bella {member.mention}, te se ama')
 
-# Skam
+# {member.mention} sei uno di noi
 
 
 @bot.command(name = comandi[10])
-async def skam(ctx):
-    await ctx.send('Nu frat a me ja')
+async def frat(ctx, member : discord.Member):
+    await ctx.send(f'{member.mention} Nu frat a me ja')
 
-# Tommaso
+# {member.mention} sei solo uno schiavo
 
 
 @bot.command(name = comandi[11])
-async def tom(ctx):
-    await ctx.send('Tommaso schiavo del governo')
+async def schiavetto(ctx, member : discord.Member):
+    await ctx.send(f'{member.mention} schiavo del governo')
 
+# Omaggio al linguaggio di programmazione
+
+
+@bot.command(name = 'Python')
+async def py(ctx):
+    await ctx.send('Grazie Python per averci permesso di fare quello che state appena vedendo')
+
+# Parte test delle nuove funzioni
+
+
+@bot.command(name = 'aiutotest')
+async def test(ctx):
+    await ctx.send(test)
+    
 bot.run(token)
